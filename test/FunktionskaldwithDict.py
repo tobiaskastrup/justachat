512e5cdee50d1e936ce5670e75a1bf6cb9a2d593
tobi844i <tobi844i@stud.kea.dk>
2021-12-07 16:13:11 +0100

    Funktionsakald med dict virker nu

--- test/FunktionskaldwithDict.py
+++ test/FunktionskaldwithDict.py
@@ -39,39 +39,41 @@ def getPublicChatRooms():
 def choosePublicRoom(_publicChannels):
     # Printer mulige rum
     print("Choose a room to connect:")
+    tempRooms = {}
     n=1
     for rooms in _publicChannels:
-        print(f'{rooms}: {rooms}')
+        tempRooms[n] = rooms
+        print(f'{n}: {tempRooms[n]}')
         n = n+1
     
-#     # Vælg et rum
-#     while True:
-#         try:
-#             i = int(input(f"Vælg et nummer mellem 1 og {len(_channelsnames)}: "))
-#             break
-#         except ValueError:
-#             print('\nYou did not enter a valid integer')
-#         if i < len(_channelsnames):
-#             print("\nFail")
-#         elif i < 1:
-#             print("\nFail")
+    # Vælg et rum
+    while True:
+        try:
+            i = int(input(f"Vælg et nummer mellem 1 og {len(_publicChannels)}: "))
+            break
+        except ValueError:
+            print('\nYou did not enter a valid integer')
+        if i < len(_publicChannels):
+            print("\nFail")
+        elif i < 1:
+            print("\nFail")
     
-#     return i, _channelsid[i-1]
+    return tempRooms[i]
 
-# def printMessages(RoomNo, _publicRoomID, _msgCount):
-#     msg = rocket.channels_history(_publicRoomID, count=_msgCount).json()
+def printMessages(_chosenPublicChannel, _chosenPublicChannelID, _msgCount):
+    msg = rocket.channels_history(_chosenPublicChannelID, count=_msgCount).json()
 
-#     # Itterer igennem beskeder fra rummet
-#     print(f"\n<<< Beskeder fra {cNames[RoomNo-1]} >>>")
-#     if "messages" in msg:
-#         msgliste = msg["messages"]
-#         if type(msgliste) is list:
-#             for xyz in reversed(msgliste):
-#                 print(f'{xyz["u"]["username"]}:     {xyz["msg"]}')
+    # Itterer igennem beskeder fra rummet
+    print(f"\n<<< Beskeder fra {_chosenPublicChannel} >>>")
+    if "messages" in msg:
+        msgliste = msg["messages"]
+        if type(msgliste) is list:
+            for xyz in reversed(msgliste):
+                print(f'{xyz["u"]["username"]}:     {xyz["msg"]}')
 
 # #### TEST AF FUNKTIONER ####
 createSession(nickname, password)
 print(getUserID("christian"))
 publicChannels = getPublicChatRooms()
-choosePublicRoom(publicChannels)
-# printMessages(chosenRoomNo, chosenPublicChatRoom, 10)
+chosenPublicChannel = choosePublicRoom(publicChannels)
+printMessages(chosenPublicChannel, publicChannels[chosenPublicChannel], 10)
